1ec932c6d11dce439175c7938732187574f1ac29
DAIBA, Keiichi <daiba@cpan.org>
2010-07-21 11:11:15 +0900

    simple deploy tool pawn

---
+++ pawn.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/env perl
+package App::Pawn::script;
+use Getopt::Long;
+use strict;
+
+sub new {
+    my $class = shift;
+    bless {
+        argv => [],
+        @_,
+    }, $class;
+}
+
+sub parse_options {
+    my $self = shift;
+    local @ARGV = @{ $self->{argv} };
+    push @ARGV, @_;
+
+    Getopt::Long::Configure("bundling");
+    Getopt::Long::GetOptions(
+        'f|file=s' => \$self->{file},
+        't|test'   => \$self->{test},
+        'h|help'   => sub { $self->help },
+    );
+    $self->{argv} = \@ARGV;
+}
+
+sub help {
+    my $self = shift;
+    die <<HELP;
+Usage: pawn [options] File
+
+Optons:
+  -h,--help       this message
+  -f,--file       specify rules directory
+  -t,--test       test mode (send no mail)
+HELP
+}
+
+sub load_file {
+    my $self = shift;
+    return unless $self->{file} && -e $self->{file};
+    my $file   = $self->{file};
+    my @attr   = qw( name hosts command eachTime endTime );
+    my $plugin = { file => $file };
+
+    my $dsl = join "\n",
+      map "sub $_ {my \$e=shift || return \$plugin->{$_}; \$plugin->{$_}=\$e }",
+      @attr;
+    my $code = do { open my $io, "<$file"; local $/; <$io> };
+
+    eval "package App::Pawn::Rule;\n" . "use strict;\nuse utf8;\n$dsl\n$code";
+    die $@ if ($@);
+}
+
+sub loop {
+    my $self = shift;
+    my @hosts = split /\s+/, App::Pawn::Rule::hosts();
+    my %ret;
+    for my $host (@hosts) {
+        next unless ($host);
+        my $fd;
+        my $command = App::Pawn::Rule::command();
+        open $fd, '-|', "ssh $host $command 2> /dev/null";
+        my $output;
+        {
+            local $/ = undef;
+            $output = <$fd>;
+        }
+        $ret{$host} = App::Pawn::Rule::eachTime()->($output);
+        close $fd;
+    }
+    App::Pawn::Rule::endTime()->( \%ret );
+}
+
+sub doit {
+    my $self = shift;
+    $self->load_file;
+    $self->loop;
+}
+
+package main;
+
+unless (caller) {
+    my $app = App::Pawn::script->new;
+    $app->parse_options(@ARGV);
+    $app->doit;
+}
